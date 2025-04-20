ab1c2094fe09cf252c3a5c01f2799461b4acdfe5
Phillip Alter <phillip.alter@outlook.com>
2025-04-20 16:08:42 -0700

    Added modifyTime function that correctly replaces times listed on NHL.com

--- scripts/content.js
+++ scripts/content.js
@@ -1,4 +1,4 @@
-console.log("Before attempt");
+// console.log("Before attempt");
 check();
 
 function check(){
@@ -6,10 +6,10 @@ function check(){
     setTimeout(()=>{
         if (els.length > 0) {
             console.log(els.length);
-            console.log("(check) Defined.");
+            // console.log("(check) Defined.");
             modifyTime(els);
         } else {
-            console.log("(check) Undefined.");
+            // console.log("(check) Undefined.");
             setTimeout(check(),1000);
         }
     },1000);
@@ -18,7 +18,10 @@ function check(){
 function modifyTime(eles){
     if (eles.length > 0){
         // console.log("(modifyTime if) before re");
-        const re = /([0-9]+):([0-9]{2})\s?([A-Z]{2})\s?([A-Za-z]{3})/g;
+        const hrs = /[0-9]{1,2}:/gi;
+        const mins = /30/gi;
+        const ampm = /(AM)|(PM)/gi;
+        const zone = /(EST)|(EDT)$/gi;
         // console.log(`(modifyTime if) re: ${re}`);
         for (var i = 0; i < eles.length; i++){
             // console.log("(modifyTime for) inside of for loop");
@@ -27,6 +30,33 @@ function modifyTime(eles){
             // console.log(`(modifyTime for) child tag name: ${cEl[0].tagName}`)
             // console.log(`(modifyTime for) child text: ${cEl[0].textContent}`)
             var str = cEl[0].textContent;
+            // console.log(`(modifyTime for) str: ${str}`)
+            var hr = str.match(hrs);
+            var min = str.match(mins);
+            var ap = str.match(ampm);
+            var tz = str.match(zone);
+
+            if (hr !== null){
+                var pdtHour = parseInt(hr[0]) - 4;
+                // console.log(`(modifyTime for if) output: ${pdtHour}`);
+                cEl[0].textContent = cEl[0].textContent.replace(hrs,pdtHour+":"); 
+            }
+            if (min !== null){
+                var pdtMins = (parseInt(min[0]) - 0).toString().padStart(2,'0');
+                // console.log(`(modifyTime for if2) output: ${pdtMins}`);
+                cEl[0].textContent = cEl[0].textContent.replace(mins,pdtMins); 
+            }
+            if (ap !== null){
+                var pdtAmpm = "PM";
+                cEl[0].textContent = cEl[0].textContent.replace(ampm,pdtAmpm); 
+            }
+            if (tz !== null){
+                var pdtZone = "PDT";
+                cEl[0].textContent = cEl[0].textContent.replace(zone,pdtZone);
+            }
         }
     }
 }
+
+
+// console.log("Outside of if statement")

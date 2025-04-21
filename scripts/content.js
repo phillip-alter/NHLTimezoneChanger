445c8aa327d8f8ba09278756564afb3f0ef2f74a
Phillip Alter <phillip.alter@outlook.com>
2025-04-21 04:47:53 +0300

    Added local storage access for DST and options check.

--- scripts/content.js
+++ scripts/content.js
@@ -1,40 +1,49 @@
-check();
 var count = 0;
 var max = 6;
-
 var offset = new Date().getTimezoneOffset();
+let finalDST;
 console.log(offset);
-var dst = isDST(new Date());
-console.log(dst);
+chrome.storage.local.get('observingDST', (data) => {
+    const userPrefersDST = data.observingDST;
+    const systemDST = isDST(new Date());
+     finalDST = systemDST && userPrefersDST;
+
+    if (finalDST) {
+        console.log("DST is active based on system and user preference.");
+    } else {
+        console.log("DST is NOT active based on system or user preference.");
+    }
+    check(finalDST);
+});
 
 function isDST(d) {
     let jan = new Date(d.getFullYear(), 0, 1).getTimezoneOffset();
     let jul = new Date(d.getFullYear(), 6, 1).getTimezoneOffset();
-    return Math.max(jan, jul) !== d.getTimezoneOffset();    
+    return Math.max(jan, jul) !== d.getTimezoneOffset();
 }
 
-function check(){
+function check(finalDST) {
     const els = document.getElementsByClassName("game-state-container");
-    setTimeout(()=>{
+    setTimeout(() => {
         if (els.length > 0) {
             console.log(els.length);
             console.log("(check) Defined.");
-            modifyTime(els);
+            modifyTime(els,finalDST);
         } else if (count < max) {
             console.log("(check) Undefined.");
             count += 1;
-            setTimeout(check(),1000);
+            setTimeout(check(), 1000);
         }
-    },1000);
+    }, 1000);
 }
 
-function modifyTime(eles){
-    if (eles.length > 0){
+function modifyTime(eles,dst) {
+    if (eles.length > 0) {
         const hrs = /[0-9]{1,2}:/gi;
         const mins = /(00)|(30)/gi;
         const ampm = /(AM)|(PM)/gi;
         const zone = /(EST)|(EDT)/gi;
-        for (var i = 0; i < eles.length; i++){
+        for (var i = 0; i < eles.length; i++) {
             var cEl = eles[i].children;
             var str = cEl[0].textContent;
             var hr = str.match(hrs);
@@ -42,9 +51,9 @@ function modifyTime(eles){
             var ampmCheck = str.match(ampm);
             var tz = str.match(zone);
             var ap;
-            if (hr !== null && min !== null && ampmCheck !== null && tz !== null){
+            if (hr !== null && min !== null && ampmCheck !== null && tz !== null) {
                 var utcTime = (parseInt(hr[0]) + 12) * 60;
-                if (dst){
+                if (dst) {
                     utcTime += 240;
                 } else {
                     utcTime += 300;
@@ -52,18 +61,18 @@ function modifyTime(eles){
                 var localTime = (utcTime) - (offset);
                 var leftoverMins = localTime % 60;
                 localTime = parseInt(localTime / 60) % 24;
-                if (localTime >= 12){
+                if (localTime >= 12) {
                     ap = "PM";
                     if (localTime !== 12) localTime = (localTime - 12);
                 } else {
                     ap = "AM";
                     if (localTime == 0) localTime += 12;
                 }
-                cEl[0].textContent = cEl[0].textContent.replace(hrs,localTime+":");
-                var minute = (Math.abs(parseInt(min[0]) - leftoverMins)).toString().padStart(2,'0');
-                cEl[0].textContent = cEl[0].textContent.replace(mins,minute); 
-                cEl[0].textContent = cEl[0].textContent.replace(ampm,ap); 
-                cEl[0].textContent = cEl[0].textContent.replace(zone,"");
+                cEl[0].textContent = cEl[0].textContent.replace(hrs, localTime + ":");
+                var minute = (Math.abs(parseInt(min[0]) - leftoverMins)).toString().padStart(2, '0');
+                cEl[0].textContent = cEl[0].textContent.replace(mins, minute);
+                cEl[0].textContent = cEl[0].textContent.replace(ampm, ap);
+                cEl[0].textContent = cEl[0].textContent.replace(zone, "");
             }
         }
     }
